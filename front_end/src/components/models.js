733697b9713364fd752963fd94bcf450607770a8
UMAKANT06 <sahuumakant877@gmail.com>
2023-10-24 19:01:13 +0530

    dev:uma changes

--- front_end/src/components/models.js
+++
@@ -1,9 +0,0 @@
-import { useLoader} from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-function Scene(){
-    const hostels=useLoader(GLTFLoader,'./assets/hostel_a.glb');
-    return <primitive object={hostels.scene} />
-}
-
-export default Scene;
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,32 +1,65 @@
+import { CubeTextureLoader } from 'three';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Cloud, OrbitControls, Environment ,Sky} from '@react-three/drei';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import { Canvas } from '@react-three/fiber';
-import Helloworld from './components/helloworld';
-import Scene from './components/models';
-function Box(){
-  return(
-    <mesh position={[-3,-1.1,2]}>
-      <boxGeometry/>
-      <meshStandardMaterial color={"red"}/>
-    </mesh>
-  )
+import { useBox, usePlane, Physics } from '@react-three/cannon';
+import { extend, useLoader,useThree } from '@react-three/fiber';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-}
-function Plane(){
-  return(
-    <mesh position={[-3,0,5]} rotation={[Math.PI/2,0,0]}>
-      <planeBufferGeometry attach={"geometry"} args={[100,100]}/>
-      <meshStandardMaterial color={"green"}/>
-    </mesh>
-  )
-}
-const root = ReactDOM.createRoot(document.getElementById('root')).render(
-  <Canvas>
-  <ambientLight intensity={0.5}/>
-  <Box/>
-</Canvas>
+extend({ OrbitControls });
 
-);
+function Skybox(){
+const {scene}=useThree();
+const loader = new CubeTextureLoader();
+const sunset = loader.load([
+  "/assets/sky/skybox_px.jpg",
+  "/assets/sky/skybox_nx.jpg",
+  "/assets/sky/skybox_py.jpg",
+  "/assets/sky/skybox_ny.jpg",
+  "/assets/sky/skybox_pz.jpg",
+  "/assets/sky/skybox_nz.jpg",
+]);
+scene.background=sunset;
+return null;
+
+};
+
+const Ground = () => {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeGeometry attach="geometry" args={[100, 100]} />
+      <meshStandardMaterial color="green" />
+    </mesh>
+  );
+};
+
+const ModelViewer = ({ modelPath, position }) => {
+  const gltf = useLoader(GLTFLoader, modelPath);
+  return <primitive object={gltf.scene} position={position} />;
+};
+
+const App = () => {
+  return (
+    <Canvas
+      style={{ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0 }}
+      camera={{ position: [0, 0, 10] }}
+    >
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      {/* <Sky sunPosition={[1, 1, 1]} /> */}
+      <OrbitControls />
+      <Physics>
+        <Skybox/>
+      <ModelViewer modelPath="/assets/hostel_a.glb" position={[10, 0, 10]} />
+      <Environment preset='sunset' />
+      </Physics>
+    </Canvas>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<App />);
