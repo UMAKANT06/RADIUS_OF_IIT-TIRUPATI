cdd93e6b899e1fac73bce68561036a14521cd490
UMAKANT06 <sahuumakant877@gmail.com>
2023-11-12 12:13:48 +0530

    dev:uma

--- front_end/src/TreesBetween2.js
+++ front_end/src/TreesBetween2.js
@@ -5,7 +5,7 @@ import { RigidBody } from "@react-three/rapier";
 export function TreesBetween2(props) {
   const { nodes, materials } = useGLTF("/assets/TreesBetween2.glb");
   return (
-    <group {...props} dispose={null} rotation={[Math.PI/2,0,0]} position={[90,0,0]} scale={[0.4,0.4,0.4]}>
+    <group {...props} dispose={null} rotation={[Math.PI/2,0,0]} position={[90,0,70]} scale={[0.2,0.2,0.2]}>
       <mesh
         castShadow
         receiveShadow
