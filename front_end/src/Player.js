9d0a01e43c1c605e188877c60d5c541d3cebb003
UMAKANT06 <sahuumakant877@gmail.com>
2023-11-18 21:42:40 +0530

    dev:changes

--- front_end/src/Player.js
+++ front_end/src/Player.js
@@ -1,6 +1,6 @@
 import * as THREE from "three"
 import * as RAPIER from "@dimforge/rapier3d-compat"
-import { useRef,useEffect } from "react"
+import { useRef, useEffect } from "react"
 import { useFrame } from "@react-three/fiber"
 import { useKeyboardControls } from "@react-three/drei"
 import { BallCollider, CapsuleCollider, CuboidCollider, RigidBody, useRapier } from "@react-three/rapier"
@@ -12,15 +12,15 @@ const sideVector = new THREE.Vector3()
 const rotation = new THREE.Vector3()
 
 export function Box(props) {
-  
+
   return (
-    
-    <RigidBody type="fixed" position={[100,1,0]}>
-      <mesh  rotation-x={-Math.PI / 2}>
-        <planeGeometry args={[5, 5,5]} />
-        <meshStandardMaterial color={"red"}/>
+
+    <RigidBody type="fixed" position={[100, 1, 0]}>
+      <mesh rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[5, 5, 5]} />
+        <meshStandardMaterial color={"red"} />
       </mesh>
-      <CuboidCollider args={[2,5,2]} />
+      <CuboidCollider args={[2, 5, 2]} />
     </RigidBody>
   )
 }
@@ -37,41 +37,20 @@ export function Player({ lerp = THREE.MathUtils.lerp }) {
     const velocity = ref.current.linvel()
 
     state.camera.position.copy(ref.current.translation())
-  
+
     frontVector.set(0, 0, backward - forward)
     sideVector.set(left - right, 0, 0)
     direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(state.camera.rotation)
     ref.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
-    ref.current.addForce({ x: 0, y: -0.1, z: 0}, true);
-    // ref.current.setLinDamping(0);
-
-  //   const world = rapier.world
-  // const ray = world.castRay(new RAPIER.Ray(ref.current.translation(), { x: 0, y: -1, z: 0 }))
-  // const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.75
-  // if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 })
-
-
-  // // raycaster.setFromCamera(coords, camera);
-  // const intersects = world.intersectCuboid(ref.current.translation(), [2, 5, 2]);
-  // if (intersects.length > 0) {
-  //   state.camera.position.set(50, 10, 0);
-  // }
-
- 
-
-
-
-
-  
-   
+    ref.current.addForce({ x: 0, y: -0.1, z: 0 }, true);
   })
 
 
- 
-  
+
+
   return (
     <>
-            <RigidBody ref={ref} colliders={false} mass={1} type="dynamic" position={[0, 10, 0]} enabledRotations={[false, false, false]}>
+      <RigidBody ref={ref} colliders={false} mass={1} type="dynamic" position={[0, 10, 0]} enabledRotations={[false, false, false]}>
         <CapsuleCollider args={[1.2, 1.2]} friction={[-0.3]} />
       </RigidBody>
 
